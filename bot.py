bbeff69feb5d8b438a5060329c647abe6c27aa3d
Naitik Sharma <147588654+theshion@users.noreply.github.com>
2024-04-30 10:19:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -103,7 +103,7 @@ class Bot(Client):
             except Exception as a:
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL4 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL4}")
+                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL5 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL4}")
                 self.LOGGER(__name__).info("\nBot Stopped. https://t.me/EonixCore for support")
                 sys.exit()
                 
