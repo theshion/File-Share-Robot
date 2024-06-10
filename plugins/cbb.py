5dc0c9cdb95badc8e934a3276df9b48e97296d7a
Naitik Sharma <147588654+theshion@users.noreply.github.com>
2024-06-11 00:33:56 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -19,7 +19,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🤖 My Name :</b> <a href='https://t.me/FileSharingHubProBot'>File Sharing Hub Bot</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢 Channel :</b> <a href='https://t.me/Demon_Slayer_S4_Dub_Hindi'> </a> \n<b>Owner :</b> <a href='tg://user?id={OWNER_ID}'>Owner</a>",
+            text = f"<b>🤖 My Name :</b> <a href='https://t.me/FileShareHubRoBot'>File Sharing Hub RoBot</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢 Channel :</b> <a href='https://t.me/Demon_Slayer_S4_Dub_Hindi'> </a> \n<b>Owner :</b> <a href='tg://user?id={OWNER_ID}'>Owner</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
